31b8a6a11e9fa65533f52f56a7d6a083b371714a
ChristofNies <christof.nies@gmail.com>
2019-06-08 20:23:52 +0200

    cleaning repo

--- Database/POC/Tables/CUSTOMERS.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE poc.customers (
-  customer_id NUMBER(10) NOT NULL,
-  firstname VARCHAR2(50 BYTE) NOT NULL,
-  lastname VARCHAR2(50 BYTE) NOT NULL,
-  postalcode NUMBER(20),
-  PRIMARY KEY (customer_id)
-);
--- Database/POC/Tables/EMPLOYEES.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE poc.employees (
-  employeeid NUMBER NOT NULL,
-  firstname VARCHAR2(50 BYTE) NOT NULL,
-  lastname VARCHAR2(50 BYTE) NOT NULL,
-  email VARCHAR2(100 BYTE),
-  officecode VARCHAR2(20 BYTE) NOT NULL,
-  job_title VARCHAR2(100 BYTE),
-  CONSTRAINT employees_pk PRIMARY KEY (employeeid)
-);
