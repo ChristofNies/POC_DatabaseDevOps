a148a1acc8e26378aeb47aacc73eb39f50ec85b7
ChristofNies <christof.nies@gmail.com>
2019-06-16 18:32:43 +0200

    altered offices

--- Liquibase/changelog.sql
+++ Liquibase/changelog.sql
@@ -6,3 +6,10 @@ ALTER TABLE Employees RENAME COLUMN EmployeeNr to EmployeeId;
 --changeset christofn:002_alter_employees
 ALTER TABLE Employees 
 ADD job_title VARCHAR2(100);
+
+--changeset christofn:003_insert_offices
+INSERT INTO Offices (OfficeCode, City, PostalCode, Address)
+VALUES ('ACNBE', 'Brussel', 1000, 'Waterloolaan 16');
+
+INSERT INTO Offices (OfficeCode, City, PostalCode, Address)
+VALUES ('ACNMU', 'Quatre Bornes', 1000, 'NeXTeracom Tower 2');
