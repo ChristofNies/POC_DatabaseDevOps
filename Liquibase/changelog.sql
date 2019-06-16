af630264c1ad26ac0c50da17cace549c30fb4bee
ChristofNies <christof.nies@gmail.com>
2019-06-16 19:15:19 +0200

    create customers

--- Liquibase/changelog.sql
+++ Liquibase/changelog.sql
@@ -13,3 +13,11 @@ VALUES ('ACNBE', 'Brussel', 1000, 'Waterloolaan 16');
 
 INSERT INTO Offices (OfficeCode, City, PostalCode, Address)
 VALUES ('ACNMU', 'Quatre Bornes', 1000, 'NeXTeracom Tower 2');
+
+--changeset christofn:004_create_customers logicalFilePath:changelog
+CREATE TABLE customers
+( customer_id number(10) NOT NULL PRIMARY KEY,
+  firstName varchar2(50) NOT NULL,
+  lastName varchar2(50) NOt NULL,
+  postalCode number(20)
+);
